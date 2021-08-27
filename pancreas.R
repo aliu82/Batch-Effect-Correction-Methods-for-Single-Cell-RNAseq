07cb7963f19ced04f9fc04fa7e8a9ad8e8b1301a
aliu82 <35516789+aliu82@users.noreply.github.com>
2021-08-27 13:18:28 -0400

    panc8 Seurat Dataset
    This Seurat dataset includes 8 human pancreas islets datasets across 5 scRNA-seq technologies.

--- pancreas.R
+++ pancreas.R
@@ -32,34 +32,34 @@ smartseq2 <- panc.list$smartseq2
 indrop <- panc.list$indrop
 
 
-gcdata <- merge(x = celseq, y = list(celseq2, fluidigmc1, smartseq2, indrop), add.cell.ids = c('celseq', 'celseq2', 'fluidigmc1', 'smartseq2', 'indrop'), merge.data = FALSE)
-gcdata <- Seurat::NormalizeData(gcdata)
-var.genes <- Seurat::SelectIntegrationFeatures(SplitObject(gcdata, split.by = 'tech'), nfeatures = 2000, fvf.nfeatures = 2000, selection.method = 'vst')
-gcdata <- Seurat::ScaleData(gcdata, features = var.genes)
-gcdata <- Seurat::RunPCA(gcdata, features = var.genes, npcs = 40, ndims.print = 1:5, nfeatures.print = 5)
-#Seurat::DimPlot(gcdata, reduction = 'pca', group.by = 'tech')
-gcdata <- Seurat::FindNeighbors(gcdata, reduction = 'pca', dims = 1:20, k.param = 20)
-gcdata <- Seurat::FindClusters(gcdata)    #used default resolution for consistency, optimal resolution = 0.1
-gcdata <- Seurat::RunUMAP(gcdata, dims = 1:20, reduction = 'pca', n.neighbors = 15, min.dist = 0.5, spread = 1, metric = 'euclidean')  
-#p1 <- Seurat::DimPlot(gcdata, reduction = 'umap', group.by = 'tech')
-#p2 <- Seurat::DimPlot(gcdata, reduction = 'umap', group.by = 'celltype')
+# raw counts data
+rcdata <- merge(x = celseq, y = list(celseq2, fluidigmc1, smartseq2, indrop), add.cell.ids = c('celseq', 'celseq2', 'fluidigmc1', 'smartseq2', 'indrop'), merge.data = FALSE)
+rcdata <- Seurat::NormalizeData(rcdata)
+var.genes <- Seurat::SelectIntegrationFeatures(SplitObject(rcdata, split.by = 'tech'), nfeatures = 2000, fvf.nfeatures = 2000, selection.method = 'vst')
+rcdata <- Seurat::ScaleData(rcdata, features = var.genes)
+rcdata <- Seurat::RunPCA(rcdata, features = var.genes, npcs = 40, ndims.print = 1:5, nfeatures.print = 5)
+#Seurat::DimPlot(rcdata, reduction = 'pca', group.by = 'tech')
+rcdata <- Seurat::FindNeighbors(rcdata, reduction = 'pca', dims = 1:20, k.param = 20)
+rcdata <- Seurat::FindClusters(rcdata)    #used default resolution for consistency, optimal resolution = 0.1
+rcdata <- Seurat::RunUMAP(rcdata, dims = 1:20, reduction = 'pca', n.neighbors = 15, min.dist = 0.5, spread = 1, metric = 'euclidean')  
+#p1 <- Seurat::DimPlot(rcdata, reduction = 'umap', group.by = 'tech')
+#p2 <- Seurat::DimPlot(rcdata, reduction = 'umap', group.by = 'celltype')
 
 # how the distribution of celltype varies per tech
 Seurat::VlnPlot(subset, 'nFeature_RNA', group.by = 'celltype') + ggplot2::theme(legend.position = 'none') + ggplot2::ggtitle('smartseq2')
-#subset <- subset(gcdata, subset = (tech == 'smartseq2'))
+#subset <- subset(rcdata, subset = (tech == 'smartseq2'))
 
 
 # ADJUSTED RAND INDEX - BATCH MIXING (lower value is better)
-ari <- dplyr::select(gcdata[[]], tech, seurat_clusters)
+ari <- dplyr::select(rcdata[[]], tech, seurat_clusters)
 ari$tech <- plyr::mapvalues(ari$tech, from = c('celseq', 'celseq2', 'fluidigmc1', 'smartseq2', 'indrop'), to = c(0, 1, 2, 3, 4))
 pdfCluster::adj.rand.index(as.numeric(ari$tech), as.numeric(ari$seurat_clusters))         #0.1485275
 
 
 # ADJUSTED RAND INDEX - CELL PURITY (higher value is better)
-ari <- dplyr::select(gcdata[[]], celltype, seurat_clusters)
+ari <- dplyr::select(rcdata[[]], celltype, seurat_clusters)
 ari$celltype <- plyr::mapvalues(ari$celltype,
-                                from = c('acinar', 'activated_stellate', 'alpha', 'beta', 'delta', 'ductal', 'endothelial', 'epsilon', 'gamma',
-                                'macrophage', 'mast', 'quiescent_stellate', 'schwann'),
+                                from = c('acinar', 'activated_stellate', 'alpha', 'beta', 'delta', 'ductal', 'endothelial', 'epsilon', 'gamma', 'macrophage', 'mast', 'quiescent_stellate', 'schwann'),
                                 to = c(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12))
 pdfCluster::adj.rand.index(as.numeric(ari$celltype), as.numeric(ari$seurat_clusters))       #0.3665525
 # optimal resolution = 0.1, ari = 0.2050022, 0.5642438
@@ -67,43 +67,42 @@ pdfCluster::adj.rand.index(as.numeric(ari$celltype), as.numeric(ari$seurat_clust
 
 # AVERAGE SILHOUETTE WIDTH (ASW)
 library(cluster, quietly = TRUE)
-dist.matrix <- dist(x = Embeddings(object = gcdata@reductions[['pca']])[, 1:30])
-sil.tech <- silhouette(x = as.numeric(x = as.factor(x = gcdata$tech)), dist = dist.matrix)
-gcdata$sil.tech <- sil.tech[, 3]
-sil.celltype <- silhouette(x = as.numeric(x = as.factor(x = gcdata$celltype)), dist = dist.matrix)
-gcdata$sil.celltype <- sil.celltype[, 3]
-mean(gcdata$sil.tech)             #0.0244474
-mean(gcdata$sil.celltype)         #0.2145548
+dist.matrix <- dist(x = Embeddings(object = rcdata@reductions[['pca']])[, 1:30])
+sil.tech <- silhouette(x = as.numeric(x = as.factor(x = rcdata$tech)), dist = dist.matrix)
+rcdata$sil.tech <- sil.tech[, 3]
+sil.celltype <- silhouette(x = as.numeric(x = as.factor(x = rcdata$celltype)), dist = dist.matrix)
+rcdata$sil.celltype <- sil.celltype[, 3]
+mean(rcdata$sil.tech)             #0.0244474
+mean(rcdata$sil.celltype)         #0.2145548
 
 ####################### SEURAT CCA #############################################
 
 panc.anchors <- Seurat::FindIntegrationAnchors(object.list = panc.list)
-panc.integrated <- Seurat::IntegrateData(anchorset = panc.anchors)
+panc.cca <- Seurat::IntegrateData(anchorset = panc.anchors)
 
 # Switch to integrated assay
-Seurat::DefaultAssay(panc.integrated) <- 'integrated'
+Seurat::DefaultAssay(panc.cca) <- 'integrated'
 # Run standard workflow for visualization and clustering
-panc.integrated <- Seurat::ScaleData(panc.integrated)
-panc.integrated <- Seurat::RunPCA(panc.integrated, npcs = 30)
-panc.integrated <- Seurat::RunUMAP(panc.integrated, reduction = 'pca', dims = 1:30)
-panc.integrated <- FindNeighbors(panc.integrated, dims = 1:10)
-panc.integrated <- FindClusters(panc.integrated)      # optimal resolution = 0.15
-#q1 <- Seurat::DimPlot(panc.integrated, reduction = 'umap', group.by = 'tech')
-#q2 <- Seurat::DimPlot(panc.integrated, reduction = 'umap', group.by = 'celltype')
-#table(Idents(panc.integrated), panc.integrated$tech)
+panc.cca <- Seurat::ScaleData(panc.cca)
+panc.cca <- Seurat::RunPCA(panc.cca, npcs = 30)
+panc.cca <- Seurat::RunUMAP(panc.cca, reduction = 'pca', dims = 1:30)
+panc.cca <- FindNeighbors(panc.cca, dims = 1:10)
+panc.cca <- FindClusters(panc.cca)      # optimal resolution = 0.15
+#q1 <- Seurat::DimPlot(panc.cca, reduction = 'umap', group.by = 'tech')
+#q2 <- Seurat::DimPlot(panc.cca, reduction = 'umap', group.by = 'celltype')
+#table(Idents(panc.cca), panc.cca$tech)
 
 
 # ADJUSTED RAND INDEX - BATCH MIXING
-ari <- dplyr::select(panc.integrated[[]], tech, seurat_clusters)
+ari <- dplyr::select(panc.cca[[]], tech, seurat_clusters)
 ari$tech <- plyr::mapvalues(ari$tech, from = c('celseq', 'celseq2', 'fluidigmc1', 'smartseq2', 'indrop'), to = c(0, 1, 2, 3, 4))
 pdfCluster::adj.rand.index(as.numeric(ari$tech), as.numeric(ari$seurat_clusters))       #0.01952463
 
 
 # ADJUSTED RAND INDEX - CELL PURITY
-ari <- dplyr::select(panc.integrated[[]], celltype, seurat_clusters)
+ari <- dplyr::select(panc.cca[[]], celltype, seurat_clusters)
 ari$celltype <- plyr::mapvalues(ari$celltype,
-                                from = c('acinar', 'activated_stellate', 'alpha', 'beta', 'delta', 'ductal', 'endothelial', 'epsilon', 'gamma',
-                                'macrophage', 'mast', 'quiescent_stellate', 'schwann'),
+                                from = c('acinar', 'activated_stellate', 'alpha', 'beta', 'delta', 'ductal', 'endothelial', 'epsilon', 'gamma', 'macrophage', 'mast', 'quiescent_stellate', 'schwann'),
                                 to = c(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12))
 pdfCluster::adj.rand.index(as.numeric(ari$celltype), as.numeric(ari$seurat_clusters))       #0.4770111
 #resolution = 0.15, ari = 0.006943822, 0.9371157
@@ -111,24 +110,26 @@ pdfCluster::adj.rand.index(as.numeric(ari$celltype), as.numeric(ari$seurat_clust
 
 # AVERAGE SILHOUETTE WIDTH (ASW)
 #library(cluster, quietly = TRUE)
-dist.matrix <- dist(x = Embeddings(object = panc.integrated@reductions[['pca']])[, 1:30])
-sil.tech <- silhouette(x = as.numeric(x = as.factor(x = panc.integrated$tech)), dist = dist.matrix)
-panc.integrated$sil.tech <- sil.tech[, 3]
-sil.celltype <- silhouette(x = as.numeric(x = as.factor(x = panc.integrated$celltype)), dist = dist.matrix)
-panc.integrated$sil.celltype <- sil.celltype[, 3]
-mean(panc.integrated$sil.tech)            #-0.1074509
-mean(panc.integrated$sil.celltype)        #0.2974331
+dist.matrix <- dist(x = Embeddings(object = panc.cca@reductions[['pca']])[, 1:30])
+sil.tech <- silhouette(x = as.numeric(x = as.factor(x = panc.cca$tech)), dist = dist.matrix)
+panc.cca$sil.tech <- sil.tech[, 3]
+sil.celltype <- silhouette(x = as.numeric(x = as.factor(x = panc.cca$celltype)), dist = dist.matrix)
+panc.cca$sil.celltype <- sil.celltype[, 3]
+mean(panc.cca$sil.tech)            #-0.1074509
+mean(panc.cca$sil.celltype)        #0.2974331
 
 
 # other indicators of measuring batch-effect, but couldn't get to work
-#lisi <- lisi::compute_lisi(panc.integrated@assays$RNA@counts, ari, c('celltype', 'seurat_clusters'))
-#kbet <- kBET::kBET(panc.integrated@assays, predictions)
+#lisi <- lisi::compute_lisi(panc.cca@assays$RNA@counts, ari, c('celltype', 'seurat_clusters'))
+#kbet <- kBET::kBET(panc.cca@assays, predictions)
 
 ####################### FAST MUTUAL NEAREST NEIGHBORS ##########################
 
 # RunFastMNN() can be found in the 'seurat-wrappers' github for Seurat
 # https://github.com/satijalab/seurat-wrappers
-# there is also a batchelor::fastMNN() function that inputs a SingleCellExperiment object instead of a Seurat object
+# api for seurat-wrappers if installation doesn't work: https://rdrr.io/github/satijalab/seurat-wrappers/api/
+# there is also a batchelor::fastMNN() function that takes in a SingleCellExperiment object instead of a Seurat object
+# but this function seems to be pretty full-proof (and a lot cleaner/simpler to work with)
 mnn.seurat <- RunFastMNN(object.list = panc.list)
 mnn.seurat <- Seurat::FindNeighbors(mnn.seurat, reduction = 'mnn', dims = 1:30)
 mnn.seurat <- Seurat::FindClusters(mnn.seurat)      # optimal resolution = 0.15
@@ -147,8 +148,7 @@ pdfCluster::adj.rand.index(as.numeric(ari$tech), as.numeric(ari$seurat_clusters)
 # ADJUSTED RAND INDEX - CELL PURITY
 ari <- dplyr::select(mnn.seurat[[]], celltype, seurat_clusters)
 ari$celltype <- plyr::mapvalues(ari$celltype,
-                                from = c('acinar', 'activated_stellate', 'alpha', 'beta', 'delta', 'ductal', 'endothelial', 'epsilon', 'gamma',
-                                'macrophage', 'mast', 'quiescent_stellate', 'schwann'),
+                                from = c('acinar', 'activated_stellate', 'alpha', 'beta', 'delta', 'ductal', 'endothelial', 'epsilon', 'gamma', 'macrophage', 'mast', 'quiescent_stellate', 'schwann'),
                                 to = c(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12))
 pdfCluster::adj.rand.index(as.numeric(ari$celltype), as.numeric(ari$seurat_clusters))       #0.6380783
 #resolution = 0.15, ari = 0.006628357, 0.9465943
@@ -168,7 +168,7 @@ mean(mnn.seurat$sil.celltype)       #0.3192418
 
 liger <- Seurat::NormalizeData(panc8)
 liger <- Seurat::FindVariableFeatures(liger)
-var.liger <- Seurat::SelectIntegrationFeatures(Seurat::SplitObject(liger, split.by = 'tech'), nfeatures = 2000, fvf.nfeatures = 2000, selection.method = 'vst')
+var.liger <- Seurat::SelectIntegrationFeatures(SplitObject(liger, split.by = 'tech'), nfeatures = 2000, fvf.nfeatures = 2000, selection.method = 'vst')
 liger <- Seurat::ScaleData(liger, split.by = 'tech', features = var.liger)
 
 # RunOptimizeALS() and RunQuantileNorm() also found in the 'seurat-wrappers' github for Seurat
@@ -191,8 +191,7 @@ pdfCluster::adj.rand.index(as.numeric(ari$tech), as.numeric(ari$seurat_clusters)
 # ADJUSTED RAND INDEX - CELL PURITY
 ari <- dplyr::select(liger[[]], celltype, seurat_clusters)
 ari$celltype <- plyr::mapvalues(ari$celltype,
-                                from = c('acinar', 'activated_stellate', 'alpha', 'beta', 'delta', 'ductal', 'endothelial', 'epsilon', 'gamma',
-                                'macrophage', 'mast', 'quiescent_stellate', 'schwann'),
+                                from = c('acinar', 'activated_stellate', 'alpha', 'beta', 'delta', 'ductal', 'endothelial', 'epsilon', 'gamma', 'macrophage', 'mast', 'quiescent_stellate', 'schwann'),
                                 to = c(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12))
 pdfCluster::adj.rand.index(as.numeric(ari$celltype), as.numeric(ari$seurat_clusters))       #0.617431
 
@@ -208,35 +207,41 @@ mean(liger$sil.tech)            #-0.1722261
 mean(liger$sil.celltype)        #0.1042689
 
 ####################### LIGER - SINGLE CELL OBJECT #############################
+# uses integrative non-negative matrix factorization to obtain a low-dimensional space
+# features batch-specific factors and shared factors
 
 plist <- list('celseq' = panc.list$celseq, 'celseq2' = panc.list$celseq2, 'fluidigmc1' = panc.list$fluidigmc1,
               'smartseq2' = panc.list$smartseq2, 'indrop' = panc.list$indrop)
-liger.panc <- rliger::createLiger(sapply(plist, function(data) data[['RNA']]@counts[, colnames(data)]))
-liger.panc <- rliger::normalize(liger.panc)
-liger.panc <- rliger::selectGenes(liger.panc)
-liger.panc <- rliger::scaleNotCenter(liger.panc)
-liger.panc <- rliger::optimizeALS(liger.panc, k=20)
-liger.panc <- rliger::quantile_norm(liger.panc)             # builds a similarity graph based on shared factor neighborhoods
-liger.panc <- rliger::louvainCluster(liger.panc)            # identify clusters shared and align quantiles within each cluster
-liger.panc <- rliger::runUMAP(liger.panc)
-all.plots <- rliger::plotByDatasetAndCluster(liger.panc, axis.labels = c('UMAP 1', 'UMAP 2'), return.plots = T)
+
+# createLiger passes in a list of gene expression matrices - used raw counts from each batch
+liger.sce <- rliger::createLiger(sapply(plist, function(data) data[['RNA']]@counts[, colnames(data)]))
+liger.sce <- rliger::normalize(liger.sce)
+liger.sce <- rliger::selectGenes(liger.sce)
+liger.sce <- rliger::scaleNotCenter(liger.sce)            # nonnegative matrix factorization requires positive values, so do not center
+liger.sce <- rliger::optimizeALS(liger.sce, k=20)         # you can use suggestK() & suggestLambda to find a more optimal k, lambda
+# H: cell loading factors
+# V: shared gene factors
+# W: dataset-specific gene factors
+liger.sce <- rliger::quantile_norm(liger.sce)             # builds a similarity graph based on shared factor neighborhoods
+liger.sce <- rliger::louvainCluster(liger.sce)            # identify clusters shared and align quantiles within each cluster
+liger.sce <- rliger::runUMAP(liger.sce)                   # runTSNE if you want TSNE graphs
+all.plots <- rliger::plotByDatasetAndCluster(liger.sce, axis.labels = c('UMAP 1', 'UMAP 2'), return.plots = T)
 all.plots[[1]]        # umap graph clustered by tech
 all.plots[[2]]        # umap graph clusters
-#plotClusterFactors(liger.panc, use.aligned = T)
+#plotClusterProportions(liger.sce)                        # shows proportion of each batch in each cluster 
 
 
 # ADJUSTED RAND INDEX - BATCH MIXING
-tech <- unlist(lapply(1:length(liger.panc@H), function(x) {rep(names(liger.panc@H)[x], nrow(liger.panc@H[[x]]))}))
-ari <- data.frame('tech' = tech, 'clusters' = liger.panc@clusters)
+tech <- unlist(lapply(1:length(liger.sce@H), function(x) {rep(names(liger.sce@H)[x], nrow(liger.sce@H[[x]]))}))
+ari <- data.frame('tech' = tech, 'clusters' = liger.sce@clusters)
 ari$tech <- plyr::mapvalues(ari$tech, from = c('celseq', 'celseq2', 'fluidigmc1', 'smartseq2', 'indrop'), to = c(0, 1, 2, 3, 4))
 pdfCluster::adj.rand.index(as.numeric(ari$tech), as.numeric(ari$clusters))        #0.01202221
 
 
 # ADJUSTED RAND INDEX - CELL PURITY
-ari <- data.frame('celltype' = panc8$celltype, 'clusters' = liger.panc@clusters)
+ari <- data.frame('celltype' = panc8$celltype, 'clusters' = liger.sce@clusters)
 ari$celltype <- plyr::mapvalues(ari$celltype,
-                                from = c('acinar', 'activated_stellate', 'alpha', 'beta', 'delta', 'ductal', 'endothelial', 'epsilon', 'gamma',
-                                'macrophage', 'mast', 'quiescent_stellate', 'schwann'),
+                                from = c('acinar', 'activated_stellate', 'alpha', 'beta', 'delta', 'ductal', 'endothelial', 'epsilon', 'gamma', 'macrophage', 'mast', 'quiescent_stellate', 'schwann'),
                                 to = c(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12))
 pdfCluster::adj.rand.index(as.numeric(ari$celltype), as.numeric(ari$clusters))    #0.2387594
 
@@ -268,4 +273,3 @@ ggplot2::ggplot(asw.df, aes(x,y)) + geom_text_repel(aes(label = names), hjust = 
         panel.border = element_blank(),
         panel.background = element_blank())
 
-                                         
